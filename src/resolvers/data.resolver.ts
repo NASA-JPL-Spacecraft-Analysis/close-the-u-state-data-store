07d83d81a2910c079709d0220553ab329b685132
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2021-09-14 09:29:23 -0700

    Added the ability to query by name

--- src/resolvers/data.resolver.ts
+++ src/resolvers/data.resolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Query, Resolver } from 'type-graphql';
 
 import { Data } from '../models';
 
@@ -7,5 +7,14 @@ export class DataResolver {
   @Query(() => [ Data ])
   public data(): Promise<Data[]> {
     return Data.find();
+  }
+
+  @Query(() => [ Data ])
+  public dataByName(@Arg('name') name: string): Promise<Data[]> {
+    return Data.find({
+      where: {
+        name
+      }
+    });
   }
 }
