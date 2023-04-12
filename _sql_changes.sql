84e8b12bf1c62324872ed17f3d763c6dd2d1fedf
auman <cydney.m.auman@jpl.nasa.gov>
2023-04-12 14:38:49 -0700

    update sql files.

--- _sql_changes.sql
+++ _sql_changes.sql
@@ -1,8 +1,2 @@
 ALTER TABLE `states` RENAME TO `state`;
 
--- ALTER TABLE `states`
--- ADD COLUMN `cpu` text DEFAULT NULL;
--- ADD COLUMN `hexId` text DEFAULT NULL;
--- ADD COLUMN `valueType` text DEFAULT NULL;
--- ADD COLUMN `version` text DEFAULT NULL;
--- ADD COLUMN `volatility` text DEFAULT NULL;
