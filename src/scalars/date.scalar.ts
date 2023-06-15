e5f30fcf31438beaceabaee955d190f13babd881
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2023-06-15 07:34:54 -1000

    Added some timezone fixes, and nvm and prettier

--- src/scalars/date.scalar.ts
+++ src/scalars/date.scalar.ts
@@ -1,19 +1,18 @@
 import { GraphQLScalarType, StringValueNode } from 'graphql';
-import format from 'date-fns/format'
-import { zonedTimeToUtc } from 'date-fns-tz'
+import { formatInTimeZone, zonedTimeToUtc } from 'date-fns-tz';
 
-const dateFormat = 'yyyy-DDD\'T\'HH:mm:ss.SSS';
+const dateFormat = "yyyy-DDD'T'HH:mm:ss.SSS";
 
 export const DateScalar = new GraphQLScalarType({
   name: 'ScetDateTime',
   description: 'Scet date time',
   serialize(value: string): string {
-    return format(new Date(value), dateFormat);
+    return formatInTimeZone(value, 'UTC', dateFormat);
   },
   parseValue(value: unknown): Date {
-    return zonedTimeToUtc(value as string, 'America/Los_Angeles');
+    return zonedTimeToUtc(value as string, 'UTC');
   },
   parseLiteral(value: unknown): Date {
-    return zonedTimeToUtc((value as StringValueNode).value, 'America/Los_Angeles');
+    return zonedTimeToUtc((value as StringValueNode).value, 'UTC');
   }
 });
