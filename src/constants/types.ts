e47fb5b9792c3dec13a1da04e00f6de3fe5e661f
Cody Hansen <cody.m.hansen@jpl.nasa.gov>
2024-05-21 12:23:35 -1000

    Changed activity instance to model input

--- src/constants/types.ts
+++ src/constants/types.ts
@@ -1,26 +1,21 @@
-import { VALUE_TYPE } from './enums'
+import { VALUE_TYPE } from './enums';
 
 export const STATE_VALUE_TYPES: Record<string, Array<String>> = {
-    channel: [VALUE_TYPE.DERIVED, VALUE_TYPE.MEASURED, VALUE_TYPE.PREDICTED],
-    fsw_parameter: [VALUE_TYPE.MEASURED, VALUE_TYPE.PREDICTED],
-    predict: [VALUE_TYPE.PREDICTED],
-    trend: [VALUE_TYPE.DERIVED]
+  channel: [VALUE_TYPE.DERIVED, VALUE_TYPE.MEASURED, VALUE_TYPE.PREDICTED],
+  fsw_parameter: [VALUE_TYPE.MEASURED, VALUE_TYPE.PREDICTED],
+  predict: [VALUE_TYPE.PREDICTED],
+  trend: [VALUE_TYPE.DERIVED]
 };
 
-
 export const STATE_TYPES: Set<string> = new Set([
-    'activity_instance',
-    'alarm_limit',
-    'channel',
-    'channel_alarm',
-    'fsw_parameter',
-    'predict',
-    'trend',
-    'user'
+  'alarm_limit',
+  'channel',
+  'channel_alarm',
+  'fsw_parameter',
+  'model_input',
+  'predict',
+  'trend',
+  'user'
 ]);
 
-export const EVENT_TYPES: Set<string> = new Set([
-    'evr',
-    'predict_event',
-    'user'
-]);
+export const EVENT_TYPES: Set<string> = new Set(['evr', 'predict_event', 'user']);
