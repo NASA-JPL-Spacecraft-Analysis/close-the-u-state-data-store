430d5ebcf6674bf6b6d68a6d9dc6ef5141b5d9bd
Dan Allard <dan@jpl.nasa.gov>
2024-10-23 12:21:20 -0700

    pushing some sql update files

--- _sql_changes.sql
+++ _sql_changes.sql
@@ -1,12 +1,5 @@
-ALTER TABLE `data` RENAME TO `states`;
 
 ALTER TABLE `states`
-ADD COLUMN `cpu` text DEFAULT NULL,
-ADD COLUMN `hexId` text DEFAULT NULL,
-ADD COLUMN `valueType` text DEFAULT NULL,
-ADD COLUMN `version` text DEFAULT NULL,
-ADD COLUMN `volatility` text DEFAULT NULL;
-ADD COLUMN `fswParamVersion` text DEFAULT NULL;
-
-UPDATE states
-SET valueType = UPPER(valueType);
+ADD COLUMN  `collectionName` text DEFAULT NULL,
+ADD COLUMN  `type` text,
+ADD COLUMN  `scetEnd` datetime(3) DEFAULT NULL;
---
+++ _sql_changes2.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `data` RENAME TO `states`;
+
+ALTER TABLE `states`
+ADD COLUMN `cpu` text DEFAULT NULL,
+ADD COLUMN `hexId` text DEFAULT NULL,
+ADD COLUMN `valueType` text DEFAULT NULL,
+ADD COLUMN `version` text DEFAULT NULL,
+ADD COLUMN `volatility` text DEFAULT NULL;
+ADD COLUMN `fswParamVersion` text DEFAULT NULL;
+
+UPDATE states
+SET valueType = UPPER(valueType);
